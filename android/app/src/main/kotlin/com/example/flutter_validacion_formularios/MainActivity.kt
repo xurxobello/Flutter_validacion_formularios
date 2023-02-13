143e91af3d673d56eea8b87aad7ad643e2973d65
xurxobello <xurxobello@hotmail.es>
2023-02-13 12:04:37 +0100

    Inicio de proyecto

---
+++ android/app/src/main/kotlin/com/example/flutter_validacion_formularios/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_validacion_formularios
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/screens.dart
@@ -0,0 +1,2 @@
+export 'package:flutter_validacion_formularios/screens/home_screen.dart';
+export 'package:flutter_validacion_formularios/screens/login_screen.dart';
